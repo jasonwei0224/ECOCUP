f7debe572f5d74273cb1475ac4492e349939e1af
sangwookYun <brtm0533@naver.com>
2020-03-23 14:30:08 -0700

    google map done

--- app/src/main/java/ca/bcit/ecocup/Maps.java
+++ app/src/main/java/ca/bcit/ecocup/Maps.java
@@ -9,15 +9,58 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Maps extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    private View view;
+public class Maps extends Fragment implements OnMapReadyCallback {
 
-    @Nullable
+    private View mView;
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+
+    public Maps() {
+
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.activity_maps_f, container, false);
-        return view;
+        mView=inflater.inflate(R.layout.activity_maps_f, container, false);
+        return mView;
     }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView=(MapView) mView.findViewById(R.id.mv_map_map);
+        if(mMapView!=null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize((getContext()));
+        mGoogleMap=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40, -74)).title("STA").snippet("asdf"));
+        CameraPosition Liberty=CameraPosition.builder().target(new LatLng(40,-74)).zoom(5).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+    }
+
+
 
 }
