6dac7663ec17017b98c227bdfc5ffb7510c2f93c
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-03-23 17:02:08 -0700

    add function to read from database

--- app/src/main/java/ca/bcit/ecocup/Points.java
+++ app/src/main/java/ca/bcit/ecocup/Points.java
@@ -15,6 +15,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Points extends Fragment {
 
@@ -22,6 +27,8 @@ public class Points extends Fragment {
     private FirebaseAuth mAuth;
     private TextView userPoints;
     ProgressBar progressBar;
+    ValueEventListener mPointsListener;
+    DatabaseReference mDatabase;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,6 +37,20 @@ public class Points extends Fragment {
         progressBar = view.findViewById(R.id.progressBar);
         userPoints = view.findViewById(R.id.userPoints);
         progressBar.setProgress(50);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        ValueEventListener pointsListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                System.out.println(dataSnapshot.getValue());
+                System.out.println("success");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mDatabase.addValueEventListener(pointsListener);
         return view;
 
     }
